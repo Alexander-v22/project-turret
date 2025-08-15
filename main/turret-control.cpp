b72b2d7a6336886440bba328cbc7eaab1e0a87ee
Alexander-v22 <av275@duke.edu>
2025-08-15 00:38:05 -0500

    Set up a very basic WS and HTTP on my ESP32 chip

--- main/turret-control.cpp
+++ main/turret-control.cpp
@@ -1,3 +1,19 @@
+/*
+=======Things to do========
+
+1.) add joystick improvemnts 
+    * joystick dead zones
+    * smoothness for contorl 
+    * calibration 
+    
+2.) improve wifi feedback
+
+
+
+
+*/
+
+
 extern "C" {
     #include "freertos/FreeRTOS.h"
     #include "freertos/task.h"
@@ -7,14 +23,18 @@ extern "C" {
     #include "esp_log.h"
 
     #include "nvs_flash.h"
+
     #include "esp_netif.h"
     #include "esp_event.h"
     #include "esp_wifi.h"
     #include "esp_http_server.h"
     #include "esp_timer.h"
-    #include "secrets.h"
     #include "esp_mac.h"
 
+    #include "sdkconfig.h"
+    
+
+    #include "secrets.h"
 }
 
 #include <cstdint>  // For uint32_t, uint8_t
@@ -48,12 +68,13 @@ extern "C" {
 // ws server settings 
 #define WS_PORT 81
 #define WS_PATH "/ws"
+static const char* TAG_WIFI = "WIFI";
+static const char *TAG_WS = "ws";
 
-static const char* TAG = "WIFI";
 
-
-// Converts servo angle (0–180°) to LEDC duty cycle (Q16 format) based on pulse width (1000–2000 us)
-uint32_t angle_to_duty_us (uint8_t angle){
+// Map angle -> pulse_us (500–2500 us at 50 Hz -> 20ms), then convert to LEDC duty counts:
+// duty = (pulse_us / 20,000 us) * 2^16  // servos respond to pulse width, not raw duty %
+uint32_t angle_to_ledc_counts (uint8_t angle){
     uint32_t us = SERVO_MIN_US + ((SERVO_MAX_US - SERVO_MIN_US) * angle ) / 180;
     return (us * (1<<16)) / (1000000/ SERVO_FREQ_HZ); // Uses linear interpolation to calculate pulse width for the current angle.
 }
@@ -65,12 +86,12 @@ uint8_t map_range(int x, int in_min, int in_max, int out_min, int out_max) {
 
 
 
-//=========== ALL OF THESE ARE SETUPS==================
+//=========== ALL OF THESE ARE SETUPS FOR SERVO USE==================
 
 // Creating a servo SET UP to free up space in the main
 static void servo_startup(uint8_t deg_pan, uint8_t deg_tilt){
-    uint32_t dpan = angle_to_duty_us(deg_pan);
-    uint32_t dtilt = angle_to_duty_us(deg_tilt);
+    uint32_t dpan = angle_to_ledc_counts(deg_pan);
+    uint32_t dtilt = angle_to_ledc_counts(deg_tilt);
 
     ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, dpan); //loads the value
     ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0); // Applies the value and sends it to the servo
@@ -119,6 +140,8 @@ static adc_oneshot_unit_handle_t s_adc1 = nullptr;
 
 // ADC SET up for joystick
 static void setup_adc(void){
+
+
     // adc1_config_width(ADC_WIDTH_BIT_12); // gives us  4095 possible values the analog value can be 
     // adc1_config_channel_atten(JOY_X_PIN, ADC_ATTEN_DB_11); // Configures the volates rand the ADC can read from 
     // adc1_config_channel_atten(JOY_Y_PIN, ADC_ATTEN_DB_11);
@@ -126,6 +149,7 @@ static void setup_adc(void){
     adc_oneshot_unit_init_cfg_t unit_cfg = {
         .unit_id = ADC_UNIT_1,
     };
+
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&unit_cfg, &s_adc1));
 
     adc_oneshot_chan_cfg_t ch_cfg = {
@@ -136,41 +160,130 @@ static void setup_adc(void){
     ESP_ERROR_CHECK(adc_oneshot_config_channel(s_adc1, (adc_channel_t)JOY_X_PIN, &ch_cfg));
     ESP_ERROR_CHECK(adc_oneshot_config_channel(s_adc1, (adc_channel_t)JOY_Y_PIN, &ch_cfg));
 }
-git 
+
 
 //=================WIFI SETUP==========================
 
 static void wifi_init_sta(void) {
-    ESP_ERROR_CHECK(nvs_flash_init()); //non-volitale storage needed for my wifi credentials to be stores to lvie through a reboot
-    ESP_ERROR_CHECK(esp_netif_init()); // prepares the network interface e.g handles ip address, network, and connections 
-    ESP_ERROR_CHECK(esp_event_loop_create_default()); // ESP32 can react to things such as wifi conected or got ip address 
-    esp_netif_create_default_wifi_sta(); // creates wifi station instead of a wifi AP mode (esp32 chip acts like a router)
+    ESP_ERROR_CHECK(nvs_flash_init()); //non-volitale storage needed for my wifi credentials to be stores to live through a reboot
 
+    /* prepares the network interface e.g handles ip address, network, and connections
+    (A.K.A TCP/IP stack)*/
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default()); //  delivers WIFI_EVENT / IP_EVENT callbaks
 
+    /* Creates the default Wi-Fi station (STA) network interface and bind it
+   to the Wi-Fi driver. */
+    esp_netif_create_default_wifi_sta(); 
+
+    // initailizes the wifi driver with default configuration 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    // THIS IS FILLING THE REQUIREMENTS NEED TO ACCESS THE WIFI 
+
+    /* Fill in STA credentials and policy. Copy SSID/password into the config struct. Sets a minimum authmode (rejects open/WEP APs) */
     wifi_config_t wifi_config = {};
         std::snprintf((char*)wifi_config.sta.ssid, sizeof(wifi_config.sta.ssid), "%s", WIFI_SSID);
         std::snprintf((char*)wifi_config.sta.password, sizeof(wifi_config.sta.password), "%s", WIFI_PASSWORD);
         wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK ;
 
-    
+    /* Puts the driver in STA(client) mode and applies the config we coded */
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));// connects to the wifi instead of staring one 
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_ERROR_CHECK(esp_wifi_connect());
-    //applyies the config, start the WIFI hardware, and attempts connections
 
-    ESP_LOGI(TAG, "Wi-Fi STA starting… connect to %s", WIFI_SSID);
+
+    ESP_LOGI(TAG_WIFI, "Wi-Fi STA starting… connect to %s", WIFI_SSID);
+}
+
+
+// Creating a basic webSocket server handler
+
+
+//==========CREATED THE ""HTTP OPENER"=============
+static httpd_handle_t g_server = nullptr;
+static esp_err_t ws_handler(httpd_req_t* req);
+
+static void start_ws_server(){
+    httpd_config_t cfg = HTTPD_DEFAULT_CONFIG();
+
+    cfg.server_port = 81;
+
+    ESP_ERROR_CHECK(httpd_start(&g_server, &cfg)); //starts a tiny web server on the ESP32 (we set the port to 81).
+    /*WebSockets run on top of HTTP. You need the HTTP server alive before any WS connection can exist.*/
+
+
+    // This is simply a URI Path like a handler 
+    static httpd_uri_t ws_uri = {};
+        ws_uri.uri = "/ws" ;
+        ws_uri.method = HTTP_GET;
+        ws_uri.handler = ws_handler;
+        ws_uri.user_ctx = nullptr;
+        ws_uri.is_websocket = true;
+
+        ESP_ERROR_CHECK(httpd_register_uri_handler(g_server,&ws_uri));
 
 }
+
+
+//===========CREATED A BASIC WEBSOCKET===========
+// simple test to make sure we can connect a WebSocket
+static esp_err_t ws_handler(httpd_req_t* req){
+
+
+    if (req ->method == HTTP_GET){
+        ESP_LOGI(TAG_WS, "WS Handshake");
+        return ESP_OK;
+    }
+
+    httpd_ws_frame_t frame = {} ;
+    frame.type = HTTPD_WS_TYPE_TEXT;
+
+    // probe length of the incomeing frame
+    ESP_ERROR_CHECK(httpd_ws_recv_frame(req, &frame, 0));
+    if (frame.len == 0 ) return ESP_OK; // no date in the frame "making sure we get a connection"
+
+    // simple fixed buffer and check bounds is need to make this work 
+  
+    uint8_t buf[256]; 
+    // allocate buffer baed on the actual frame length 
+    if(frame.len >= sizeof(buf)){
+        ESP_LOGW(TAG_WS, "Frame to large: %u max(%u)", (unsigned)frame.len, (unsigned)sizeof(buf) - 1);
+        return ESP_ERR_INVALID_SIZE;
+    }    
+
+    frame.payload = buf;
+    ESP_ERROR_CHECK(httpd_ws_recv_frame(req, &frame,frame.len));
+    buf[frame.len] = 0;
+
+    //PRINT OUT RESULT 
+    ESP_LOGI(TAG_WS, "RX: %s", buf);
+
+
+    //simpley getting the same text back
+    httpd_ws_frame_t tx_frame = {};
+        tx_frame.final=true;
+        tx_frame.type = HTTPD_WS_TYPE_TEXT;
+        tx_frame.payload = buf;
+        tx_frame.len = frame.len;
+    ESP_ERROR_CHECK(httpd_ws_send_frame(req, &tx_frame));
+
+    return ESP_OK;
+
+}
+
+
+
 
 extern "C" void app_main(void) {
 
     setup_ledc();
     setup_adc();
+
+    wifi_init_sta();
+    start_ws_server();
 
 
     //MAP JOYSTICK POSITION TO SERVO ANGLE 
@@ -196,9 +309,9 @@ extern "C" void app_main(void) {
         uint8_t pan_angle = map_range(joy_x, 0 , 4095, 0 ,180);
         uint8_t tilt_angle = map_range(joy_y, 0, 4095, 0, 180);
 
-        //convers angles to PWM duty (Q16 format) and APPLy every loop to continue enidng the PWM singal to the ESP32 
-        uint32_t duty_pan = angle_to_duty_us(pan_angle); //converts the angle to a PWM signal 
-        uint32_t duty_tilt = angle_to_duty_us(tilt_angle);  
+        //convers angles to PWM duty (Q16 format) and is applied every loop to continue sending the PWM singal to the ESP32 
+        uint32_t duty_pan = angle_to_ledc_counts(pan_angle); //converts the angle to a PWM signal 
+        uint32_t duty_tilt = angle_to_ledc_counts(tilt_angle);  
 
         servo_startup(pan_angle, tilt_angle);
 
